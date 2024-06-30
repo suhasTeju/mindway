10c5d1ae01ebb086a9920bc5a578f05c2710c7b3
suhasr <suhas22.st@gmail.com>
2024-06-30 22:42:28 +0530

    update product.tsx

--- components/UI/product.tsx
+++ components/UI/product.tsx
@@ -33,7 +33,7 @@ export const Product = ({
                 <img
                   src={imgSrc}
                   alt={itemName}
-                  className="max-lg:mx-auto lg:ml-auto h-full sm:object-cover md:object-contain"
+                  className="max-lg:mx-auto lg:ml-auto h-full sm:object-contain md:object-contain"
                 />
                 {totalReview == "0" && (
                   <div
