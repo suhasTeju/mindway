93ee99d285c9da4db7f68bf2ef372652143d08c0
suhasr <suhas22.st@gmail.com>
2024-07-15 22:33:29 +0530

    update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Mindway Biomedical Pvt Ltd",
+  name: "Mindway Bio Medicals Pvt Ltd",
   description:
     "High-Quality Biomedical Equipment to Empower Your Diagnostic Center",
   navItems: [
